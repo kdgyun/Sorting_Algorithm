ba4123e2cdea7fb5ed2dfe1632342a4d553dd979
kdgyun <73222685+kdgyun@users.noreply.github.com>
2021-08-01 21:49:16 +0900

    implements sorting of primitive types array

--- SortingAlgorithm/Java/BinaryInsertionSort/BinaryInsertionSort.java
+++ SortingAlgorithm/Java/BinaryInsertionSort/BinaryInsertionSort.java
@@ -14,6 +14,278 @@ package BinaryInsertionSort;
 
 public class BinaryInsertionSort {
 
+	
+
+	// ============= primitive type ============//
+
+	/*
+	 * ========================== 
+	 *  sorting byte type array
+	 * ==========================
+	 */
+	
+	public static void sort(byte[] a) {
+		if(a.length < 2) {
+			return;
+		}
+		int incLength = getAscending(a, 0, a.length);
+		sort(a, 0, a.length, incLength);
+	}
+
+
+	private static void sort(byte[] a, int lo, int hi ,int start) {
+
+		if(lo == start) {
+			start++;
+		}
+
+		for (; start < hi; start++) {
+			byte target = a[start];
+
+			int loc = binarySearch(a, target, 0, start);
+
+			int j = start - 1;
+
+			while (j >= loc) {
+				a[j + 1] = a[j];
+				j--;
+			}
+
+			a[loc] = target;
+		}
+	}
+
+	private static int binarySearch(byte[] a, byte key, int lo, int hi) {
+
+		int mid;
+		while (lo < hi) {
+			mid = (lo + hi) >>> 1;
+			if (key < a[mid]) {
+				hi = mid;
+			} 
+			else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+
+	private static int getAscending(byte[] a, int lo, int hi) {	
+
+		int limit = lo + 1;
+		if (limit == hi) { 
+			return 1;
+		}
+
+		if (a[lo] <= a[limit]) {
+			while (limit < hi && a[limit - 1] <= a[limit]) {
+				limit++;
+			}
+		}
+		else {
+			while (limit < hi && a[limit - 1] > a[limit]) {
+				limit++;
+			}
+			reversing(a, lo, limit);
+		}
+
+		return limit - lo;
+	}
+	
+	private static void reversing(byte[] a, int lo, int hi) {
+		hi--;
+		while (lo < hi) {
+			byte temp = a[lo];
+			a[lo++] = a[hi];
+			a[hi--] = temp;
+		}
+	}
+	
+	
+	
+	
+	
+	/*
+	 * ========================== 
+	 *  sorting char type array
+	 * ==========================
+	 */
+	
+	public static void sort(char[] a) {
+		if(a.length < 2) {
+			return;
+		}
+		int incLength = getAscending(a, 0, a.length);
+		sort(a, 0, a.length, incLength);
+	}
+
+
+	private static void sort(char[] a, int lo, int hi ,int start) {
+
+		if(lo == start) {
+			start++;
+		}
+
+		for (; start < hi; start++) {
+			char target = a[start];
+
+			int loc = binarySearch(a, target, 0, start);
+
+			int j = start - 1;
+
+			while (j >= loc) {
+				a[j + 1] = a[j];
+				j--;
+			}
+
+			a[loc] = target;
+		}
+	}
+
+	private static int binarySearch(char[] a, char key, int lo, int hi) {
+
+		int mid;
+		while (lo < hi) {
+			mid = (lo + hi) >>> 1;
+			if (key < a[mid]) {
+				hi = mid;
+			} 
+			else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+
+	private static int getAscending(char[] a, int lo, int hi) {	
+
+		int limit = lo + 1;
+		if (limit == hi) { 
+			return 1;
+		}
+
+		if (a[lo] <= a[limit]) {
+			while (limit < hi && a[limit - 1] <= a[limit]) {
+				limit++;
+			}
+		}
+		else {
+			while (limit < hi && a[limit - 1] > a[limit]) {
+				limit++;
+			}
+			reversing(a, lo, limit);
+		}
+
+		return limit - lo;
+	}
+	
+	private static void reversing(char[] a, int lo, int hi) {
+		hi--;
+		while (lo < hi) {
+			char temp = a[lo];
+			a[lo++] = a[hi];
+			a[hi--] = temp;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	/*
+	 * ========================== 
+	 *  sorting short type array
+	 * ==========================
+	 */
+	
+	public static void sort(short[] a) {
+		if(a.length < 2) {
+			return;
+		}
+		int incLength = getAscending(a, 0, a.length);
+		sort(a, 0, a.length, incLength);
+	}
+
+
+	private static void sort(short[] a, int lo, int hi ,int start) {
+
+		if(lo == start) {
+			start++;
+		}
+
+		for (; start < hi; start++) {
+			short target = a[start];
+
+			int loc = binarySearch(a, target, 0, start);
+
+			int j = start - 1;
+
+			while (j >= loc) {
+				a[j + 1] = a[j];
+				j--;
+			}
+
+			a[loc] = target;
+		}
+	}
+
+	private static int binarySearch(short[] a, short key, int lo, int hi) {
+
+		int mid;
+		while (lo < hi) {
+			mid = (lo + hi) >>> 1;
+			if (key < a[mid]) {
+				hi = mid;
+			} 
+			else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+
+	private static int getAscending(short[] a, int lo, int hi) {	
+
+		int limit = lo + 1;
+		if (limit == hi) { 
+			return 1;
+		}
+
+		if (a[lo] <= a[limit]) {
+			while (limit < hi && a[limit - 1] <= a[limit]) {
+				limit++;
+			}
+		}
+		else {
+			while (limit < hi && a[limit - 1] > a[limit]) {
+				limit++;
+			}
+			reversing(a, lo, limit);
+		}
+
+		return limit - lo;
+	}
+	
+	private static void reversing(short[] a, int lo, int hi) {
+		hi--;
+		while (lo < hi) {
+			short temp = a[lo];
+			a[lo++] = a[hi];
+			a[hi--] = temp;
+		}
+	}
+	
+	
+	
+	
+	
+	/*
+	 * ========================== 
+	 *  sorting int type array
+	 * ==========================
+	 */
+	
 	public static void sort(int[] a) {
 		if(a.length < 2) {
 			return;
@@ -90,6 +362,268 @@ public class BinaryInsertionSort {
 			a[hi--] = temp;
 		}
 	}
+	
+	
+	
+	
+	
+	
+	/*
+	 * ========================== 
+	 *  sorting long type array
+	 * ==========================
+	 */
+	
+	public static void sort(long[] a) {
+		if(a.length < 2) {
+			return;
+		}
+		int incLength = getAscending(a, 0, a.length);
+		sort(a, 0, a.length, incLength);
+	}
 
+
+	private static void sort(long[] a, int lo, int hi ,int start) {
+
+		if(lo == start) {
+			start++;
+		}
+
+		for (; start < hi; start++) {
+			long target = a[start];
+
+			int loc = binarySearch(a, target, 0, start);
+
+			int j = start - 1;
+
+			while (j >= loc) {
+				a[j + 1] = a[j];
+				j--;
+			}
+
+			a[loc] = target;
+		}
+	}
+
+	private static int binarySearch(long[] a, long key, int lo, int hi) {
+
+		int mid;
+		while (lo < hi) {
+			mid = (lo + hi) >>> 1;
+			if (key < a[mid]) {
+				hi = mid;
+			} 
+			else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+
+	private static int getAscending(long[] a, int lo, int hi) {	
+
+		int limit = lo + 1;
+		if (limit == hi) { 
+			return 1;
+		}
+
+		if (a[lo] <= a[limit]) {
+			while (limit < hi && a[limit - 1] <= a[limit]) {
+				limit++;
+			}
+		}
+		else {
+			while (limit < hi && a[limit - 1] > a[limit]) {
+				limit++;
+			}
+			reversing(a, lo, limit);
+		}
+
+		return limit - lo;
+	}
+	
+	private static void reversing(long[] a, int lo, int hi) {
+		hi--;
+		while (lo < hi) {
+			long temp = a[lo];
+			a[lo++] = a[hi];
+			a[hi--] = temp;
+		}
+	}
+	
+	
+	
+	
+	
+	/*
+	 * ========================== 
+	 *  sorting float type array
+	 * ==========================
+	 */
+	
+	public static void sort(float[] a) {
+		if(a.length < 2) {
+			return;
+		}
+		int incLength = getAscending(a, 0, a.length);
+		sort(a, 0, a.length, incLength);
+	}
+
+
+	private static void sort(float[] a, int lo, int hi ,int start) {
+
+		if(lo == start) {
+			start++;
+		}
+
+		for (; start < hi; start++) {
+			float target = a[start];
+
+			int loc = binarySearch(a, target, 0, start);
+
+			int j = start - 1;
+
+			while (j >= loc) {
+				a[j + 1] = a[j];
+				j--;
+			}
+
+			a[loc] = target;
+		}
+	}
+
+	private static int binarySearch(float[] a, float key, int lo, int hi) {
+
+		int mid;
+		while (lo < hi) {
+			mid = (lo + hi) >>> 1;
+			if (key < a[mid]) {
+				hi = mid;
+			} 
+			else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+
+	private static int getAscending(float[] a, int lo, int hi) {	
+
+		int limit = lo + 1;
+		if (limit == hi) { 
+			return 1;
+		}
+
+		if (a[lo] <= a[limit]) {
+			while (limit < hi && a[limit - 1] <= a[limit]) {
+				limit++;
+			}
+		}
+		else {
+			while (limit < hi && a[limit - 1] > a[limit]) {
+				limit++;
+			}
+			reversing(a, lo, limit);
+		}
+
+		return limit - lo;
+	}
+	
+	private static void reversing(float[] a, int lo, int hi) {
+		hi--;
+		while (lo < hi) {
+			float temp = a[lo];
+			a[lo++] = a[hi];
+			a[hi--] = temp;
+		}
+	}
+	
+	
+	
+	
+	
+	
+	/*
+	 * ========================== 
+	 *  sorting double type array
+	 * ==========================
+	 */
+	
+	public static void sort(double[] a) {
+		if(a.length < 2) {
+			return;
+		}
+		int incLength = getAscending(a, 0, a.length);
+		sort(a, 0, a.length, incLength);
+	}
+
+
+	private static void sort(double[] a, int lo, int hi ,int start) {
+
+		if(lo == start) {
+			start++;
+		}
+
+		for (; start < hi; start++) {
+			double target = a[start];
+
+			int loc = binarySearch(a, target, 0, start);
+
+			int j = start - 1;
+
+			while (j >= loc) {
+				a[j + 1] = a[j];
+				j--;
+			}
+
+			a[loc] = target;
+		}
+	}
+
+	private static int binarySearch(double[] a, double key, int lo, int hi) {
+
+		int mid;
+		while (lo < hi) {
+			mid = (lo + hi) >>> 1;
+			if (key < a[mid]) {
+				hi = mid;
+			} 
+			else {
+				lo = mid + 1;
+			}
+		}
+		return lo;
+	}
+
+	private static int getAscending(double[] a, int lo, int hi) {	
+
+		int limit = lo + 1;
+		if (limit == hi) { 
+			return 1;
+		}
+
+		if (a[lo] <= a[limit]) {
+			while (limit < hi && a[limit - 1] <= a[limit]) {
+				limit++;
+			}
+		}
+		else {
+			while (limit < hi && a[limit - 1] > a[limit]) {
+				limit++;
+			}
+			reversing(a, lo, limit);
+		}
+
+		return limit - lo;
+	}
+	
+	private static void reversing(double[] a, int lo, int hi) {
+		hi--;
+		while (lo < hi) {
+			double temp = a[lo];
+			a[lo++] = a[hi];
+			a[hi--] = temp;
+		}
+	}
 }
 
